2a4defe18c9a6b0f31925932808f0e9c589a2fb5
Karthik Ayangar <66073214+passionate-programmer-kapil@users.noreply.github.com>
2021-06-06 23:46:16 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>
+        This was an amazing bootcamp conducted by shapeAI. Shaurya Sir has taught as basics of Javascript and React js. This bootcamp is really helpful for all the students to get started with react framework.
+      </p>
+</div>
+  );
+}
+
+export default Note;
